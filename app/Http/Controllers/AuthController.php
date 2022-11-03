bf0707070d4db49b2ead97fa6d2ade3f9167c3da
Vitu <mafeniblessed@gmail.com>
2022-11-03 15:38:25 +0900

    new_auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,117 +2,99 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class AuthController extends Controller
 {
-    /**
-     * Create a new AuthController instance.
-     *
-     * @return void
-     */
+
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => ['login']]);
+        $this->middleware('auth:api', ['except' => ['login','register']]);
     }
 
-    /**
-     * Get a JWT via given credentials.
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function login()
+    public function login(Request $request)
     {
-//        $credentials = request(['email', 'password']);
+        $request->validate([
+            'phone' => 'required|string',
+            'password' => 'required|string',
+        ]);
+        $credentials = $request->only('phone', 'password');
 
-        //input validation
-        if (!isset(request(['password'])['password']) && (!isset(request(['email'])['email']) || !isset(request(['phone'])['phone']))) {
-            return response()->json(['isError' => true, 'message' => 'Both fields are required, please check input!'], 201);
+        $token = Auth::attempt($credentials);
+        if (!$token) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Unauthorized',
+            ], 401);
         }
 
-        if (!empty(request(['email'])['email'])) {
-            $credentials = request(['email', 'password']);
-        } else {
-            $credentials = request(['phone', 'password']);
-        }
-
-        if (!$token = auth()->attempt($credentials)) {
-            return response()->json(['isError' => true, 'message' => "Wrong username or password!"], 401);
-        }
-        $json = $this->respondWithToken($token)->getData();
-        $user = $this->me()->getData();
-        $access_token = $json->access_token;
-        $result = array();
-
-        $result['access_token'] = $access_token;
-        $result['user_id'] = $user->user_id;
-        $result['first_name'] = $user->first_name;
-        $result['last_name'] = $user->last_name;
-        $result['phone'] = $user->phone;
-        $result['email'] = $user->email;
-        $result['profile_img'] = $user->profile_img;
-        $result['is_active'] = $user->is_active;
-        $result['is_verified'] = $user->is_verified;
-
-        if ($user->is_active != 1) {
-            return response()->json(['isError' => true, 'message' => "Your account is in an inactive state, contact admin!"], 201);
-        }
-
-        if ($user->is_verified != 1) {
-            return response()->json(['isError' => true, 'message' => "Verify your account first to proceed, if message persists, contact admin !"], 201);
-        }
-
-        return response()->json(['isError' => false, 'message' => "Login successful!", "user_data" => $result], 200);
-
-
-    }
-
-    /**
-     * Get the authenticated User.
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function me()
-    {
-        return response()->json(auth()->user());
-    }
-
-    /**
-     * Log the user out (Invalidate the token).
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function logout()
-    {
-        auth()->logout();
-
-        return response()->json(['message' => 'Successfully logged out']);
-    }
-
-    /**
-     * Refresh a token.
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function refresh()
-    {
-        return $this->respondWithToken(auth()->refresh());
-    }
-
-    /**
-     * Get the token array structure.
-     *
-     * @param string $token
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    protected function respondWithToken($token)
-    {
+        $user = Auth::user();
         return response()->json([
-            'access_token' => $token,
-            'token_type' => 'bearer',
-            'expires_in' => auth()->factory()->getTTL() * 60
+            'status' => 'success',
+            'user' => $user,
+            'authorisation' => [
+                'token' => $token,
+                'type' => 'bearer',
+            ]
+        ]);
+
+    }
+
+    public function register(Request $request){
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = Auth::login($user);
+        return response()->json([
+            'status' => 'success',
+            'message' => 'User created successfully',
+            'user' => $user,
+            'authorisation' => [
+                'token' => $token,
+                'type' => 'bearer',
+            ]
         ]);
     }
+
+    public function logout()
+    {
+        Auth::logout();
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Successfully logged out',
+        ]);
+    }
+
+    public function me()
+    {
+        return response()->json([
+            'status' => 'success',
+            'user' => Auth::user(),
+        ]);
+    }
+
+    public function refresh()
+    {
+        return response()->json([
+            'status' => 'success',
+            'user' => Auth::user(),
+            'authorisation' => [
+                'token' => Auth::refresh(),
+                'type' => 'bearer',
+            ]
+        ]);
+    }
+
 }
